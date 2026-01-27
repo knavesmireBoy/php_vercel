43f03b0f41c1c092be98ad886a43441d485c3bdc
Andrew Sykes <andrewsykes@btinternet.com>
2026-01-27 16:10:19 +0000

    girl

--- api/templates/head.html.php
+++ api/templates/head.html.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en">
 <head>
-	<meta charset="utf-8"><!-- IE compatibility mode issue -->
+	<meta charset="utf-8">
 	<meta content="IE=edge" http-equiv="X-UA-Compatible">
 	<meta content="width=device-width, initial-scale=1" name="viewport">
-	<base href="phpvercel-silk.vercel.app/">
 	<title><?= $title ?? 'Artist List'; ?></title>
 	<link href="css/basic.css" media="screen" rel="stylesheet" type="text/css">
 	<script>
