5e4513f7cdc25a7cd66ab7f1eb39d93a72d5c7b9
Andrew Sykes <andrewsykes@btinternet.com>
2026-01-27 15:59:51 +0000

    base1

--- api/templates/head.html.php
+++ api/templates/head.html.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8"><!-- IE compatibility mode issue -->
 	<meta content="IE=edge" http-equiv="X-UA-Compatible">
 	<meta content="width=device-width, initial-scale=1" name="viewport">
-	<base href="/">
+	<base href="phpvercel-silk.vercel.app/">
 	<title><?= $title ?? 'Artist List'; ?></title>
 	<link href="css/basic.css" media="screen" rel="stylesheet" type="text/css">
 	<script>
--- api/templates/landing.html.php
+++ api/templates/landing.html.php
@@ -1,7 +1,7 @@
 <?php
 //https://phpvercel-silk.vercel.app/
 include_once "head.html.php"; ?>
-<h3>Please select an artist 15:58</h3>
+<h3>Please select an artist 16:00</h3>
 <form name="cds" method="post" action="">
   Name of Artist: <select name="artist">
     <?php
