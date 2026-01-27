17b8b4b82db20165524cc68bd6d22edff6c6d510
Andrew Sykes <andrewsykes@btinternet.com>
2026-01-27 17:48:09 +0000

    slash

--- api/templates/head.html.php
+++ api/templates/head.html.php
@@ -5,7 +5,7 @@
 	<meta content="IE=edge" http-equiv="X-UA-Compatible">
 	<meta content="width=device-width, initial-scale=1" name="viewport">
 	<title><?= $title ?? 'Artist List'; ?></title>
-	<link href="css/basic.css" media="screen" rel="stylesheet" type="text/css">
+	<link href="basic.css" media="screen" rel="stylesheet" type="text/css">
 	<script>
 	      document.cookie='resolution='+Math.max(screen.width,screen.height)+'; path=/'; 
 	</script>
--- api/templates/landing.html.php
+++ api/templates/landing.html.php
@@ -1,7 +1,7 @@
 <?php
 //https://phpvercel-silk.vercel.app/
 include_once "head.html.php"; ?>
-<h3>Please select an artist 17:39</h3>
+<h3>Please select an artist 17:48</h3>
 <form name="cds" method="post" action="">
   Name of Artist: <select name="artist">
     <?php
