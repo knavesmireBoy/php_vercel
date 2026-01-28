f8b0d64a75d67da7a3dc0d08906f3723b8d881f3
Andrew Sykes <andrewsykes@btinternet.com>
2026-01-28 09:38:48 +0000

    public_outer

--- templates/head.html.php
+++ templates/head.html.php
@@ -6,7 +6,7 @@
 	<meta content="IE=edge" http-equiv="X-UA-Compatible">
 	<meta content="width=device-width, initial-scale=1" name="viewport">
 	<title><?= $title ?? 'Artist List'; ?></title>
-	<link href="/public/css/basic.css" media="screen" rel="stylesheet" type="text/css">
+	<link href="css/basic.css" media="screen" rel="stylesheet" type="text/css">
 	<script>
 	      document.cookie='resolution='+Math.max(screen.width,screen.height)+'; path=/'; 
 	</script>
