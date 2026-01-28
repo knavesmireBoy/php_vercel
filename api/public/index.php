fcbc720f09d2f3dadeac55fe35df28acf763c746
Andrew Sykes <andrewsykes@btinternet.com>
2026-01-28 09:19:53 +0000

    head

--- api/public/index.php
+++ api/public/index.php
@@ -78,7 +78,7 @@ if (isset($_GET['sort'])) {
     while ($row = $result->fetch()) {
         $artists[$row['id']] = $row['artist'];
     }
-    include __DIR__ . '../templates/head.html.php';
+    include __DIR__ . '/../../templates/head.html.php';
 
     include __DIR__ . '/../../templates/landing.html.php';
 }
