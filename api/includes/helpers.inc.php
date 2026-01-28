c5e40e0df91bc9d6b65c84078c9ffc47a63bbcf0
Andrew Sykes <andrewsykes@btinternet.com>
2026-01-28 14:14:15 +0000

    eroor

--- api/includes/helpers.inc.php
+++ api/includes/helpers.inc.php
@@ -25,7 +25,7 @@ function doQuery($pdo, $sql, $msg)
         return $pdo->query($sql);
     } catch (PDOException $e) {
         $error = $msg . ' ' . $e->getMessage();
-        include 'error.html.php';
+        include '../error.html.php';
         exit();
     }
 }
@@ -36,7 +36,7 @@ function doPreparedQuery($st, $msg)
         return $st->execute();
     } catch (PDOException $e) {
         $error = $msg . ' ' . $e->getMessage();
-        include './error.html.php';
+        include '../error.html.php';
         exit();
     }
 }
