e9ba97e7eabdcaec50762cfe7c9e0ce9dfa2858d
Andrew Sykes <andrewsykes@btinternet.com>
2026-01-28 10:52:24 +0000

    favicon

--- templates/head.html.php
+++ templates/head.html.php
@@ -1,14 +1,20 @@
-
 <!DOCTYPE html>
 <html class="no-js" lang="en">
+
 <head>
 	<meta charset="utf-8">
 	<meta content="IE=edge" http-equiv="X-UA-Compatible">
 	<meta content="width=device-width, initial-scale=1" name="viewport">
 	<title><?= $title ?? 'Artist List'; ?></title>
-	<link href="css/basic.css" media="screen" rel="stylesheet" type="text/css">
+	<link href="/css/basic.css" media="screen" rel="stylesheet" type="text/css">
+	<link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+	<link rel="manifest" href="/favicon/site.webmanifest" />
 	<script>
-	      document.cookie='resolution='+Math.max(screen.width,screen.height)+'; path=/'; 
+		document.cookie = 'resolution=' + Math.max(screen.width, screen.height) + '; path=/';
 	</script>
 </head>
+
 <body>
